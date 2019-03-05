1dbe953ab097756b1f35e1992bbbf6cab930b732
Nic Grounds <nic@nicandnat.com>
2019-03-04 21:14:57 -0600

    Move Test class, add JButton and JLabel as example.

--- Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test
-{
-    public static void main( final String[] args )
-    {
-        final JFrame frame = new JFrame();
-        frame.setVisible( true );
-    }
-}
---
+++ src/Test.java
@@ -0,0 +1,31 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Test
+{
+    public static void main( final String[] args )
+    {
+        final JFrame frame = new JFrame( "Title" );
+        frame.setSize( 400, 500 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        frame.setLayout( new BorderLayout() );
+        final JButton b = new JButton( "Click Me" );
+        final JLabel l = new JLabel();
+        b.addActionListener( new ActionListener() {
+            public void actionPerformed( final ActionEvent e )
+            {
+                l.setText( "You clicked the button" );
+            }
+        } );
+        frame.add( b, BorderLayout.NORTH );
+        frame.add( l, BorderLayout.SOUTH );
+
+        frame.setVisible( true );
+    }
+}
