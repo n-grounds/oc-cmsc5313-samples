924f510c7c275d13bba6d21adc0012b8d08ae67b
Nic Grounds <nic@nicandnat.com>
2019-03-04 21:30:30 -0600

    Open JButton to show Open JFileChooser dialog

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Test
 {
@@ -29,6 +32,24 @@ public class Test
         } );
         frame.add( b, BorderLayout.NORTH );
         frame.add( l, BorderLayout.SOUTH );
+
+        final JButton open = new JButton( "Open" );
+        open.addActionListener( new ActionListener() {
+            public void actionPerformed( final ActionEvent e )
+            {
+                //JOptionPane has lots of basic dialogs
+                final JFileChooser chooser = new JFileChooser();
+                final int result = chooser.showOpenDialog( frame );
+                if( result == JFileChooser.APPROVE_OPTION ) {
+                    final File f = chooser.getSelectedFile();
+                    JOptionPane.showMessageDialog(
+                        frame, f.getAbsolutePath(),
+                        "Selection",
+                        JOptionPane.INFORMATION_MESSAGE );
+                }
+            }
+        } );
+        frame.add( open, BorderLayout.WEST );
 
         final JLabel imgLabel = new JLabel();
         imgLabel.setIcon( new ImageIcon( "steve.jpg" ) );
