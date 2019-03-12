07b08c8214fc311e3d24e2be914dd0f57547e643
Nic Grounds <nic@nicandnat.com>
2019-03-11 20:25:41 -0500

    Initial commit of basic driver

---
+++ src/java/Driver.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Driver
+{
+    public static void main( final String[] args )
+    {
+        JFrame frame = new JFrame( "MVC Demo" );
+        frame.setSize( 500, 500 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        frame.setVisible( true );
+    }
+}
