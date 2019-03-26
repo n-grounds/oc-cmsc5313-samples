3d626be824db367bd11732790803918b7d792af9
Nic Grounds <nic@nicandnat.com>
2019-03-25 19:36:23 -0500

    Add Button with callback to update Label

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -27,9 +32,17 @@ public class Test
         exit.setOnAction( e -> {
             primaryStage.close();
         } );
+
+        final Button b = new Button( "Click me" );
+        final Pane bPane = new HBox( b );
+        bPane.setPadding( new Insets( 50, 0, 30, 50 ) );
+        final Label l = new Label();
+        b.setOnAction( e -> {
+            l.setText( "You clicked the button" );
+        } );
         
         primaryStage.setScene( new Scene(
-            new VBox( bar ),
+            new VBox( bar, bPane, l ),
             500, 400 ) );
         primaryStage.show();
     }
