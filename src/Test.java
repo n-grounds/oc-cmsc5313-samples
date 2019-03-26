c927ff01a8e92d173ad42d09c4ffe7f353a73b32
Nic Grounds <nic@nicandnat.com>
2019-03-25 19:28:58 -0500

    Add menubar to application.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,8 +19,17 @@ public class Test
         throws Exception
     {
         primaryStage.setTitle( "JavaFX Test" );
+        final Menu file = new Menu( "File" );
+        final MenuBar bar = new MenuBar( file );
+        // bar.getMenus().add( file );
+        final MenuItem exit = new MenuItem( "Exit" );
+        file.getItems().add( exit );
+        exit.setOnAction( e -> {
+            primaryStage.close();
+        } );
+        
         primaryStage.setScene( new Scene(
-            new VBox(),
+            new VBox( bar ),
             500, 400 ) );
         primaryStage.show();
     }
