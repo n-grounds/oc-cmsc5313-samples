235482b27b4bf82345bfbd9627f383dfa17084c7
Nic Grounds <nic@nicandnat.com>
2019-04-15 20:20:55 -0500

    Change button to add new element to HTML with value from text field.

--- renderer.js
+++ renderer.js
@@ -3,9 +3,17 @@ const electron = require('electron');
 
 document.getElementById('my-btn')
     .addEventListener('click', () => {
-        electron.ipcRenderer.send(
-            'some-call',
-            document.getElementById('my-text').value);
+        // electron.ipcRenderer.send(
+        //     'some-call',
+        //     document.getElementById('my-text').value);
         // console.log('User clicked button');
+        const v = document.getElementById('my-text').value;
+        const div = document.getElementById('content');
+        //add new HTML element to div with the my-text value
+        //as it's content
+        const span = document.createElement('span');
+        span.appendChild(document.createTextNode( v ));
+        div.appendChild(span);
+        document.getElementById('my-text').value = '';
     });
 
