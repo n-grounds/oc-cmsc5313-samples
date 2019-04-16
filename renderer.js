684adaed00c32743a4e6cf7d9b11e32f0c34e5ed
Nic Grounds <nic@nicandnat.com>
2019-04-15 20:51:06 -0500

    Rename app title to TODO app and add checkbox for each added todo item

--- renderer.js
+++ renderer.js
@@ -12,6 +12,9 @@ document.getElementById('my-btn')
         //add new HTML element to div with the my-text value
         //as it's content
         const element = document.createElement('div');
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        element.appendChild(checkbox);
         element.appendChild(document.createTextNode( v ));
         div.appendChild(element);
         document.getElementById('my-text').value = '';
