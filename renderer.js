330978775fb4f7a0df5381ff2fb3276d6c4b3363
Nic Grounds <nic@nicandnat.com>
2019-04-15 20:16:34 -0500

    Use text input field to allow user to input something to be logged on main process console

--- renderer.js
+++ renderer.js
@@ -4,7 +4,8 @@ const electron = require('electron');
 document.getElementById('my-btn')
     .addEventListener('click', () => {
         electron.ipcRenderer.send(
-            'some-call', 'User clicked button');
+            'some-call',
+            document.getElementById('my-text').value);
         // console.log('User clicked button');
     });
 
